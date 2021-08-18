454588e7925ed5a6cd15e2191b608c2f7a5c58f9
Kevin Ho <hokevin78@gmail.com>
2021-08-18 20:24:37 +0800

    added username for direct message

--- direct_message/serializers.py
+++ direct_message/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 from .models import Direct
+from django.contrib.auth.models import User
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["username","id"]
 class DMSerializer(serializers.ModelSerializer):
-
+    sender = UserSerializer(many=False)
+    recipient = UserSerializer(many=False)
     class Meta:
         model = Direct
         fields = ('sender', 'recipient', 'dm')
