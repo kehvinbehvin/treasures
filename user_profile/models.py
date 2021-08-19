82f32911ebf2048aa3fa7ae71501be7a04f35eea
Kevin Ho <hokevin78@gmail.com>
2021-08-19 15:52:02 +0800

    made changes to user-profile model

--- user_profile/models.py
+++ user_profile/models.py
@@ -7,8 +7,8 @@ class UserProfile(models.Model):
     friends = models.ManyToManyField(User, related_name="friend_id", blank=True)
     nickname = models.CharField(max_length= 20, default="Null")
     address = models.CharField(max_length= 20, default="Null")
-    age = models.IntegerField(blank=True, default=0)
+    age = models.IntegerField(blank=True, null=True)
     about_me = models.CharField(max_length=1000, default="Null")
-    image_src = models.CharField(max_length=1000, default="https://c.files.bbci.co.uk/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg")
+    image_src = models.CharField(max_length=1000)
     
     
