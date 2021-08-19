a3a5394a4180b26fd55dd277f85d37dac2c4ede9
Kevin Ho <hokevin78@gmail.com>
2021-08-19 20:35:32 +0800

    added username for hunters in treasures

--- treasure/serializers.py
+++ treasure/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 from .models import Treasure
+from django.contrib.auth.models import User
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["username","id"]
 class TreasuresSerializer(serializers.ModelSerializer):
-
+    hunters = UserSerializer(many=True)
     class Meta:
         model = Treasure
         fields = ('author', 'name','description', 'lng','lat','date','hunters','quiz','mcq1','mcq2','mcq3','hint','answer')
