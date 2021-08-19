e4b420dac7f793c2e4a9630c4e363f52b4e8f62c
Kevin Ho <hokevin78@gmail.com>
2021-08-19 16:36:40 +0800

    revert changes to the user-profile model

--- user_profile/models.py
+++ user_profile/models.py
@@ -7,7 +7,7 @@ class UserProfile(models.Model):
     friends = models.ManyToManyField(User, related_name="friend_id", blank=True)
     nickname = models.CharField(max_length= 20, default="Null")
     address = models.CharField(max_length= 20, default="Null")
-    age = models.IntegerField(blank=True, null=True)
+    age = models.IntegerField()
     about_me = models.CharField(max_length=1000, default="Null")
     image_src = models.CharField(max_length=1000)
     
