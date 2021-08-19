de88431afb56206d11749bbaa1eabc99c80d8314
Kevin Ho <hokevin78@gmail.com>
2021-08-19 15:41:56 +0800

    added defaults for user-profile because required option is not working

--- user_profile/models.py
+++ user_profile/models.py
@@ -5,10 +5,10 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     friends = models.ManyToManyField(User, related_name="friend_id", blank=True)
-    nickname = models.CharField(max_length= 20, blank= True)
-    address = models.CharField(max_length= 20, blank= True)
-    age = models.IntegerField(blank=True)
-    about_me = models.CharField(max_length=1000,blank=True)
+    nickname = models.CharField(max_length= 20, default="Null")
+    address = models.CharField(max_length= 20, default="Null")
+    age = models.IntegerField(blank=True, default=0)
+    about_me = models.CharField(max_length=1000, default="Null")
     image_src = models.CharField(max_length=1000, default="https://c.files.bbci.co.uk/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg")
     
     
