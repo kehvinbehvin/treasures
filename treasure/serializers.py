4e0b66294f6cecf05514fad977c042adcd11dd0b
Kevin Ho <hokevin78@gmail.com>
2021-08-19 20:39:08 +0800

    resolved hunteres bug

--- treasure/serializers.py
+++ treasure/serializers.py
@@ -6,8 +6,10 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["username","id"]
+
 class TreasuresSerializer(serializers.ModelSerializer):
     hunters = UserSerializer(many=True)
     class Meta:
         model = Treasure
         fields = ('author', 'name','description', 'lng','lat','date','hunters','quiz','mcq1','mcq2','mcq3','hint','answer')
+        depth = 1
